3af910fc0a8389d7d418ff118620887115acbb0d
Kira Yukhno <kira.yukhno123@gmail.com>
2026-01-12 22:45:16 +0200

    fix the structure #2

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
-import NoteDetailsClient from '@/app/notes/NoteDetails.client';
+import NoteDetailsClient from './NoteDetails.client';
 
 interface NoteDetailsPageProps {
   params: {
